5b6e2b615f64a49e7de493b8c40b3256fb8a5ab6
Mike <wl02599509@gmail.com>
2023-07-09 00:26:54 +0800

    AllStocks class to create stocks data in json

---
+++ all_stocks.rb
@@ -0,0 +1,22 @@
+require_relative 'request_api'
+require_relative 'search_date'
+
+class AllStocks
+  # 列出 search_date 中，所有股票的資訊
+  def get_all_stock_within_date
+    url = "https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX?date=#{SearchDate.new.date}&type=ALLBUT0999&response=json"
+
+    stacks_json = RequestApi.new(url).parsed_json
+
+    select_stack_part = stacks_json['tables'].select do |part|
+      next if part['title'].nil?
+      part['title'].include?('每日收盤行情(全部(不含權證、牛熊證))')
+    end.first
+
+    stacks = select_stack_part['data']
+    needed_field_idx = [0, 1, 8]
+    stacks.map do |stack|
+      stack.keep_if.with_index { |_v, field_idx| needed_field_idx.include?(field_idx) }
+    end
+  end
+end
