e51cc35c2d8188dd70f78b12e9cbe81912513719
Mike <wl02599509@gmail.com>
2023-07-09 22:45:30 +0800

    add new unneeded stock: all_biotech_medical_code

--- stock_crawler.rb
+++ stock_crawler.rb
@@ -3,7 +3,7 @@ require_relative 'unneeded_stocks'
 
 # [股票代號、股票名稱、該日收盤價]
 all_stocks = AllStocks.new.get_all_stock_within_date
-unneeded_stocks = UnneededStocks.new.all_etf_code
+unneeded_stocks = UnneededStocks.new.all_unneeded_stocks_code
 needed_stock = all_stocks.reject! { |stock| unneeded_stocks.include? stock.first }
 
-p needed_stock
+p needed_stock.count
