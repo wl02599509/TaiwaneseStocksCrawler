ec5cfb85f68f34dc88aa9858a5e8896b5e68285b
Mike <wl02599509@gmail.com>
2023-07-09 23:37:54 +0800

    refactor AllStocks method

--- all_stocks.rb
+++ all_stocks.rb
@@ -4,19 +4,31 @@ require_relative 'search_date'
 class AllStocks
   # 列出 search_date 中，所有股票的資訊
   def get_all_stock_within_date
+    filter_needed_field
+  end
+
+  private
+
+  def stocks_page_json
     url = "https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX?date=#{SearchDate.new.latest_trade_date}&type=ALLBUT0999&response=json"
 
     stacks_json = RequestApi.new(url).parsed_json
+  end
 
-    select_stack_part = stacks_json['tables'].select do |part|
+  def all_stocks_json
+    stocks_page_json['tables'].select do |part|
       next if part['title'].nil?
+
       part['title'].include?('每日收盤行情(全部(不含權證、牛熊證))')
     end.first
+  end
 
-    stacks = select_stack_part['data']
+  def filter_needed_field
+    stacks = all_stocks_json['data']
     needed_field_idx = [0, 1, 8]
     stacks.map do |stack|
       stack.keep_if.with_index { |_v, field_idx| needed_field_idx.include?(field_idx) }
     end
   end
+
 end
--- stock_crawler.rb
+++ stock_crawler.rb
@@ -6,4 +6,4 @@ all_stocks = AllStocks.new.get_all_stock_within_date
 unneeded_stocks = UnneededStocks.new.all_unneeded_stocks_code
 needed_stock = all_stocks.reject! { |stock| unneeded_stocks.include? stock.first }
 
-p needed_stock.count
+p needed_stock
